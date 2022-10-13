5891d849e946506d21ee2dfedfe345295a321b38
aidangarrity <79874830+aidangarrity@users.noreply.github.com>
2022-10-13 16:26:52 +0100

    Pone Strategy 3

--- proj2.R
+++ proj2.R
@@ -1,4 +1,4 @@
-Pone <- function(n, k, strategy, nreps.nreps){
+Pone <- function(n, k, strategy, nreps){
   # Function to estimate the probability a single prisoner succeeding in finding their number.
   # Input:  n: a natural number
   #         k: the prisoner’s number
@@ -6,10 +6,20 @@ Pone <- function(n, k, strategy, nreps.nreps){
   #         nreps.nreps: the number of replicate simulations to run in order to estimate the probability
   # Output: prob: brobability of a single prisoner succeeding in finding their number.
   if (strategy == 1) {
-    initial_box = k
+    initial_box <- k
   }
   else if (strategy ==2) {
-    initial_box = sample(1:2*n, size=1)
+    initial_box <- sample(1:2*n, size=1)
+  }
+  else if (strategy == 3){
+    number_found <- 0
+    for (i in 1:nreps){
+      boxes <- sample(1:(2*n), n)
+      if (k %in% boxes){
+        number_found <- number_found + 1
+      }
+    }
+    return(number_found / nreps)
   }
 }
 
